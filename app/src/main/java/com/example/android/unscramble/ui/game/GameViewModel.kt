ce229ba550d2dc62afa9c5fa58764530fcc445c7
kaan.okyay <kaan.okyay@rci.rogers.ca>
2022-03-03 13:03:42 -0500

    Implemented submit button and error handling

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -46,6 +46,21 @@ class GameViewModel : ViewModel() {
         } else false
     }
 
+    private fun increaseScore(){
+        _score += SCORE_INCREASE
+    }
+
+    fun isUserWordCorrect(playerWord:String) : Boolean{
+
+        if(playerWord.equals(currentWord,true)){
+            increaseScore()
+            return true
+        }
+        return false
+    }
+
+
+
 
     override fun onCleared() {
         super.onCleared()
